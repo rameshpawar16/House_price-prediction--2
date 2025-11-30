fb3eebfd2d7ac999c308d86f4c91ee2f727c2b3c
rameshpawa <your.email@example.com>
2025-11-30 22:30:57 +0530

    commit for continous monitoring

---
+++ src/monitor.py
@@ -0,0 +1,30 @@
+import json
+from src.model import HousePrice
+
+THRESHOLD = 0.90  
+
+def monitor():
+    hp = HousePrice()
+    hp.model()
+
+    metrics = hp._evaluate_on_file("data/test.csv")
+    accuracy = metrics["r2"]
+
+    print("Current accuracy:", accuracy)
+
+    if accuracy < THRESHOLD:
+        print("⚠ ALERT: Model accuracy dropped!")
+
+        hp.model()
+        print("New model trained and ready to deploy")
+
+        with open("alert.json", "w") as f:
+            json.dump({"alert": True}, f)
+
+    else:
+        print("Accuracy OK")
+        with open("alert.json", "w") as f:
+            json.dump({"alert": False}, f)
+
+if __name__ == "__main__":
+    monitor()
